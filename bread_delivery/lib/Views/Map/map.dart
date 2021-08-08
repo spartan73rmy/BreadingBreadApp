1ad22af43cf80036d68390c2b45ee04f257e65c4
José Alberto Espinoza Morelos <spartan73rmy12@gmail.com>
2021-08-07 20:10:59 -0500

    Mapa

--- bread_delivery/lib/Views/Map/map.dart
+++ bread_delivery/lib/Views/Map/map.dart
@@ -1,15 +1,32 @@
 import 'package:bread_delivery/Entities/storePoint.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapArea extends StatefulWidget {
-  MapArea({Key key}) : super(key: key);
+  final LatLng center;
+  final List<StorePoint> points;
 
+  MapArea(this.center, this.points, {Key key}) : super(key: key);
   @override
   _MapAreaState createState() => _MapAreaState();
 }
 
 class _MapAreaState extends State<MapArea> {
+  var markers = <Marker>[];
+
+  @override
+  void initState() {
+    markers.addAll(widget.points.map((e) => Marker(
+        width: 40.0,
+        height: 40.0,
+        point: e.point,
+        builder: (ctx) => Container(
+              child: FlutterLogo(),
+            ))));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,26 +34,14 @@ class _MapAreaState extends State<MapArea> {
       height: MediaQuery.of(context).size.height * 0.35,
       child: FlutterMap(
         options: MapOptions(
-          center: widget.point,
+          center: widget.center,
           zoom: 15.0,
         ),
         layers: [
           TileLayerOptions(
               urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
               subdomains: ['a', 'b', 'c']),
-          MarkerLayerOptions(
-            markers: [
-              //TODO pasar una lista de puntos y hacer los marcadores
-              Marker(
-                width: 40.0,
-                height: 40.0,
-                point: widget.point,
-                builder: (ctx) => Container(
-                  child: FlutterLogo(),
-                ),
-              ),
-            ],
-          ),
+          MarkerLayerOptions(markers: markers)
         ],
       ),
     );
